eea984b4d2bfc5cb176ff219f3021f6a1a140229
Artyom Nesin <artem.nesin@gmail.com>
2026-06-18 17:45:11 +0400

    fix: allow roles in UpdateUserDto to fix 400 on user update

--- src/modules/core/users/dto/update-user.dto.ts
+++ src/modules/core/users/dto/update-user.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
-import { IsBoolean, IsOptional } from 'class-validator';
+import { ArrayNotEmpty, IsBoolean, IsIn, IsOptional } from 'class-validator';
+import { Role } from '../../../../generated/client';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
@@ -8,4 +9,9 @@ export class UpdateUserDto extends PartialType(
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
+
+  @IsOptional()
+  @ArrayNotEmpty()
+  @IsIn(Object.values(Role), { each: true })
+  roles?: Role[];
 }
